14d1cc2375b1c90d7367eb5f1042b2d245ba80db
ulempicka <urszula.lempicka@gmail.com>
2020-08-05 17:51:51 +0200

    Commit 3 - fixed according to comments

--- src/numbersFromStream/TestStream.java
+++ src/numbersFromStream/TestStream.java
@@ -1,33 +1,14 @@
 package numbersFromStream;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class TestStream {
     public static void main(String[] args) {
 
-        Stream<Integer> numbers = Stream.iterate(8, number -> number + 1);
-
-        numbers.filter(new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer number) {
-                return (number > 100) && (number % 5 == 0);
-            }
-        })
-                .map(new Function<Integer, Integer>() {
-                    @Override
-                    public Integer apply(Integer number) {
-                        return number * 3;
-                    }
-                })
+        Stream.iterate(8, number -> number + 1)
+                .filter(number -> (number > 100) && (number % 5 == 0))
+                .map(number -> number * 3)
                 .limit(10)
-                .forEach(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer number) {
-                        System.out.println(number);
-                    }
-                });
+                .forEach(System.out::println);
     }
 }
